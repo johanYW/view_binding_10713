e182d610c7e7742bfe17dcca05adaaa89ed79239
johanYW <johanwahyu49@gmail.com>
2022-09-12 23:57:22 +0700

    Selesai

---
+++ app/src/main/java/com/example/view_binding_10713/TaskList.kt
@@ -0,0 +1,12 @@
+package com.example.view_binding_10713
+
+object TaskList {
+    val  taskList = listOf<Task> (
+        Task("Chapter 1","Bangun Tidur","Kegiatan yang dilakukan saat seteleh tidur"),
+        Task("Chapter 2", "Bercocok Tanam", "Kegiatan yang sangat mengasikkan dengan menanam tumbuhan"),
+        Task("Chapter 3","Dolan","Berpergian keliling daerah tanpa tau arah dan tujuan demi menghilangkan stress tugas"),
+        Task("Chapter 4","Santuy","Merehatkan pikiran sejenah dari semua beban pikiran"),
+        Task("Chapter 5","Explore Internet", "Mencari informasi menarik yang ada di internet dengan tujuan tertentu")
+    )
+
+}
